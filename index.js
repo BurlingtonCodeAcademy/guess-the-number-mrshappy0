6a495fb609ba6a304269ae285b9685ddab0611b7
mrshappy0 <51244673+mrshappy0@users.noreply.github.com>
2019-06-10 12:11:48 -0400

    started project

--- index.js
+++ index.js
@@ -1,18 +1,66 @@
-const readline = require('readline');
+const readline = require("readline");
 const rl = readline.createInterface(process.stdin, process.stdout);
 
 function ask(questionText) {
-  return new Promise((resolve, reject) => {
-    rl.question(questionText, resolve);
-  });
+	return new Promise((resolve, reject) => {
+		rl.question(questionText, resolve);
+	});
 }
 
 start();
 
 async function start() {
-  console.log("Let's play a game where you (human) make up a number and I (computer) try to guess it.")
-  let secretNumber = await ask("What is your secret number?\nI won't peek, I promise...\n");
-  console.log('You entered: ' + secretNumber);
-  // Now try and complete the program.
-  process.exit();
+	let lowerBound = 0;
+	let higherBound = 100;
+	let guess = average(higherBound, lowerBound);
+	console.log(
+		"Let's play a game where you (human) make up a number and I (computer) try to guess it."
+	);
+	let yesOrNo = await ask("Is it " + guess + "? ");
+
+	while (yesOrNo !== "yes") {
+		console.log("You entered: " + yesOrNo);
+
+		let lowOrHigh = await ask("Is it lower or higher? ");
+		console.log("You entered: " + lowOrHigh);
+		if (yesOrNo === "yes") {
+			console.log("winner, winner, chicken dinner!");
+			process.exit();
+		}
+		if (yesOrNo !== "yes" && lowOrHigh === "higher") {
+			lowerBound = defineLow(lowerBound, guess);
+			guess = average(higherBound, lowerBound);
+			//console.log(guess, higherBound, lowerBound);
+		} else if (yesOrNo !== "yes" && lowOrHigh === "lower") {
+			higherBound = defineHigh(higherBound, guess);
+			guess = average(higherBound, lowerBound);
+			//console.log(guess, higherBound, lowerBound);
+		} else {
+			console.log("What's wrong with you human!? Try again!");
+		}
+		yesOrNo = await ask("Is it " + guess + "? ");
+	}
+	console.log("winner, winner, chicken dinner!");
+	process.exit();
+}
+
+function defineLow(lowerBound, guess) {
+	return lowerBound + guess;
+}
+
+function defineLow2(lowerBound, guess) {
+	return lowerBound + guess / 2;
+}
+
+function defineHigh(higherBound, guess) {
+	return higherBound - guess;
+}
+
+function defineHigh2(higherBound, guess) {
+	return higherBound - guess / 2;
+}
+
+function average(higherBound, lowerBound) {
+	let number = (higherBound + lowerBound) / 2;
+	return Math.ceil(number);
 }
